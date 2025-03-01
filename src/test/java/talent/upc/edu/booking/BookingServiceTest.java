e5c53233c4135144564d52d12a660c6fdac953e9
xescuder <xescuder@gmail.com>
2025-03-01 20:08:29 +0100

    Added first booking service test of calculateTotalPrice (no BookingService exists yet)

---
+++ src/test/java/talent/upc/edu/booking/BookingServiceTest.java
@@ -0,0 +1,35 @@
+package talent.upc.edu.booking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import talent.upc.edu.booking.model.User;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BookingServiceTest {
+    private BookingService bookingService;
+
+    @BeforeEach
+    void setup() {
+        this.bookingService = new BookingService();
+    }
+
+    @Test
+    void should_ReturnCorrectTotalPrice_When_MultipleNightsAreBooked() {
+        // Given
+        User user = new User(1L, "Xavier", "Escudero", "xescuder@gmail.com", null);
+        LocalDate dateFrom = LocalDate.of(2025, 4, 21);
+        LocalDate dateTo = LocalDate.of(2025, 4, 25);
+        BookingRequest bookingRequest = new BookingRequest(user, dateFrom, dateTo, 100);
+        int expected = Period.between(dateFrom, dateTo).getDays() * bookingRequest.getPricePerNight();
+
+        // When
+        int actual = bookingService.calculateTotalPrice(pricePerNight, numberOfNights);
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
+    }
+}
