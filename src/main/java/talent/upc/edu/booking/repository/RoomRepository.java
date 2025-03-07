c7ccdf3e5da1a07f8b4d2ef0b5885d41cb349561
xescuder <xescuder@gmail.com>
2025-03-07 16:08:11 +0100

    Added RoomRepository

---
+++ src/main/java/talent/upc/edu/booking/repository/RoomRepository.java
@@ -0,0 +1,17 @@
+package talent.upc.edu.booking.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import talent.upc.edu.booking.model.Room;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface RoomRepository extends CrudRepository<Room, Long> {
+    List<Room> findAll();
+
+    @Query("SELECT r FROM Room r WHERE r.capacity >= ?1 AND r.id NOT IN " +
+            "( SELECT br.room.id FROM Booking br WHERE ((br.checkInDate <= ?2) AND (br.checkInDate >= ?3)) )")
+    List<Room> findAvailableRooms(int totalGuests, LocalDate checkInDate, LocalDate checkOutDate);
+}
+
